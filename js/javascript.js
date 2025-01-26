e966f5df9831c371295470beff5d4816fdb7af7f
Henri Gicquel <gicquel.henri@gmail.com>
2025-01-26 15:28:54 +0100

    Correctifs

--- js/javascript.js
+++ js/javascript.js
@@ -2,16 +2,17 @@ document.addEventListener("DOMContentLoaded", () => {
   async function loadJSON() {
     try {
       const params = new URLSearchParams(location.search);
-      const reference = "KH"; //params.get("url");
+      const reference = params.get("url");
       const response = await fetch("../json/database.json");
       const data = await response.json();
       document.getElementById("title").textContent = data[reference].Name;
+      document.getElementById("Timeline").textContent = data[reference].Timeline;
       document.getElementById("Histoire").textContent = data[reference].Histoire;
       document.getElementById("Dev").textContent = data[reference].Dev;
       document.getElementById("Avis").textContent = data[reference].Avis;
       document.getElementById("Sources").textContent = data[reference].Sources;
       document.getElementById("Plateformes").textContent = data[reference].Plateformes;
-      document.getElementById("AchatImage").setAttribute("href", "" + data[reference].Achat); //https://www.cdiscount.com/search/kingdom+hearts+ps2.html
+      document.getElementById("AchatImage").setAttribute("href", "" + data[reference].Achat);
       if (data[reference].Cover) {
         document.getElementById("GameImage").setAttribute("src", "" + data[reference].Cover);
         document.getElementById("GameImage").style.display = "block";
@@ -20,12 +21,6 @@ document.addEventListener("DOMContentLoaded", () => {
       console.error("Error loading JSON:", error);
     }
   }
-
-  function toggleBlock(id) {
-    const block = document.getElementById(id);
-    block.classList.toggle("collapsed");
-  }
-
   // Load JSON on page load
   loadJSON();
 });
